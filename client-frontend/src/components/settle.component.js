7ef689c72764dfcfddc11c9681cc9b55959b0b5a
Mihai Burlaciuc <mihaiburlaciuc@gmail.com>
2020-04-19 13:02:46 +0300

    updated settle page

--- client-frontend/src/components/settle.component.js
+++ client-frontend/src/components/settle.component.js
@@ -7,14 +7,72 @@ export default class SettleComponent extends Component {
       super(props);
 
 			this.state = {
-        username: props.history.location.state.username,
         token: props.history.location.state.token,
-        isVendorChecked: false
+        isVendorChecked: false,
+        entityName: '',
+        amount: 0
       }
     }
 
     handleChange(evt) {
       this.setState({ isVendorChecked: evt.target.checked });
+    }
+
+    onChangeEntityName(e) {
+      this.setState({
+        entityName: e.target.value
+      })
+    }
+
+    onChangeAmount(e) {
+      this.setState({
+        amount: e.target.value
+      })
+    }
+
+    onClickSettle() {
+      let serverURL = 'http://localhost:8080/users/addConflict';
+      let operation;
+      let dstType;
+
+      if (this.state.isVendorChecked) {
+        // SETTLING
+        operation = 4;
+        dstType = 'vendor';
+      } else {
+        // LENTING
+        operation = 3;
+        dstType = 'user';
+      }
+
+      let addConflictReq = {
+        token: this.state.token,
+        operation: operation,
+        srcEntity: this.state.username,
+        dstEntity: this.state.targetUsername,
+        srcOwedAmount: this.state.amount,
+        srcType: "user",
+        dstType: dstType
+      };
+
+      console.log("onClickSettle: addConflictReq" , addConflictReq);
+
+      axios.post(serverURL, addConflictReq)
+      .then(response => {
+        console.log("addConflict Respose", response.status, response.data);
+        
+        this.props.history.push(
+          "/main",
+          {
+            username: this.state.username,
+            token: this.state.token
+          }
+        );
+      })
+      .catch(err => {
+        console.log("catch Err");
+        console.log(err);
+      });
     }
 
     render() {
@@ -27,7 +85,10 @@ export default class SettleComponent extends Component {
 								<h4>Send to:</h4>
 							</Form.Label>
 							<Col>
-								<Form.Control type="text" placeholder="target username/vendor" />
+                <Form.Control type="text" placeholder="target username/vendor"
+                value = { this.state.entityName }
+                onChange ={ e => this.onChangeEntityName(e) } />
+                 />
 							</Col>
 						</Form.Row>
             <Form.Row>
@@ -42,7 +103,11 @@ export default class SettleComponent extends Component {
 								<h4>Amount</h4>
 							</Form.Label>
 							<Col>
-								<Form.Control type="text" placeholder="amount" />
+                <Form.Control 
+                  value = { this.state.amount }
+                  onChange = { e => this.onChangeAmount(e) }
+                  type="number"
+                  placeholder="amount" />
 							</Col>
 							<Col>
 							<InputGroup.Append>
@@ -56,17 +121,8 @@ export default class SettleComponent extends Component {
 					<Button 
 						variant="outline-primary"
 						block
-						onClick={() => {
-								// TODO: axios call to server
-								this.props.history.push(
-									"/main",
-									{
-										username: this.state.username,
-										token: this.state.token
-									}
-								);
-							}}
-						>Send</Button>{' '}
+						onClick={() => this.onClickSettle()}
+						>Settle</Button>{' '}
         </Container>
       );
     }
